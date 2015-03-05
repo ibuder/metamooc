88d22011b5368b80690e90f390289ae51c6e2dab
Immanuel Buder <ibuder@gmail.com>
2015-03-05 12:00:55 -0500

    add data reading to db.py

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ Created on Wed Mar  4 14:50:31 2015
 """
 
 import sqlalchemy
+import pandas as pd
 
 import password
 
@@ -20,3 +21,10 @@ def get_rw_engine():
     return sqlalchemy.create_engine(
     'mysql+mysqldb://metamooc_rw:' + password.password +
     '@localhost/metamooc?charset=utf8&use_unicode=0')
+
+
+def get_coursera_ratings():
+    return pd.read_sql(
+    'SELECT * FROM coursera_courses AS t1 JOIN coursetalk_avg_ratings' +
+    ' AS t2 ON t1.name = t2.title',
+    get_ro_engine())
