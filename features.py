af0cd96dfdc1ec464e8bee25780fe2af2e91d34e
Immanuel Buder <ibuder@gmail.com>
2015-03-06 13:51:22 -0500

    Add features

--- features.py
+++ features.py
@@ -44,9 +44,31 @@ class CourseraFeatures():
         subtitle_lists = [s.split(',') for s in subtitle_strings]
         # Get the unique languages        
         langs = [l for l in itertools.chain.from_iterable(subtitle_lists)]
-        langs = list(unique(langs))  # Needs to be a list to remove the ''
+        langs = list(np.unique(langs))  # Needs to be a list to remove the ''
         langs.remove('')
         has_lang = dict()  # Map languages to which course has subtitles in it
         for lang in langs:
             has_lang[lang] = [lang in l for l in subtitle_lists]
         return pd.DataFrame(has_lang)
+        
+    def target_audience(self):
+        return self.Xraw['targetAudience']
+        
+    def rating(self):
+        return self.Xraw['rating']
+    
+    def n_rating(self):
+        return self.Xraw['n_rating']
+        
+    def feature_set1(self):
+        result = self.subtitle_languages()
+        result['average_hours'] = self.average_hours()
+        result['targetAudience'] = self.target_audience()
+        result['n_rating'] = self.n_rating()
+        return result
+        
+    def feature_set(self):
+        """
+        Get current 'best' feature set--may change w/o warning
+        """
+        return self.feature_set1()
