36f08fac889606af0004aadc0dfc1d6c52bafba6
Immanuel Buder <ibuder@gmail.com>
2015-03-07 17:32:57 +0000

    Add subtitle language to form

--- flask_app.py
+++ flask_app.py
@@ -4,12 +4,31 @@ app = Flask('metamooc')
 import wtforms.form
 import wtforms.validators as validators
 import wtforms.fields
+import babel
+
+import features
+cf = features.CourseraFeatures()
+
+
+def map_lang_form(langs):
+    """
+    Utility to map a list of language codes to (code, human-readable) pairs
+    """
+    def pair_builder(lang):
+        loc = babel.Locale.parse(lang.replace('_', '-'), sep='-')
+        return (lang, loc.english_name + ' (' + loc.get_language_name() + ')',)
+
+    return map(pair_builder, langs)
+
 
 class RecommendWeightForm(wtforms.form.Form):
     average_hours = wtforms.fields.DecimalField(u'Time commitment', [validators.required()])
     targetAudience = wtforms.fields.DecimalField(u'Difficulty', [validators.required()])
     n_rating = wtforms.fields.DecimalField(u'Popularity', [validators.required()])
-
+    subtitle_lang = wtforms.fields.SelectField(u'Subtitle language', 
+        [validators.optional()], choices=map_lang_form(cf.subtitle_languages().columns))
+    subtitle_weight = wtforms.fields.DecimalField(u'Subtitle importance',
+        [validators.optional()], default=0.0)
 
 @app.route("/", methods=['GET', 'POST'])
 def recommend():
