141ebf4f930cfbf47f4703f28adf0abd85edcda9
Immanuel Buder <ibuder@gmail.com>
2015-03-07 17:08:53 -0500

    Add content-based recommendations on first features

--- features.py
+++ features.py
@@ -9,6 +9,7 @@ import numpy as np
 import re
 import itertools
 import pandas as pd
+import sklearn.preprocessing as skpreprocessing
 
 import db
 
@@ -60,15 +61,51 @@ class CourseraFeatures():
     def n_rating(self):
         return self.Xraw['n_rating']
         
-    def feature_set1(self):
+    def feature_set1(self, normalize=False):
+        """
+        normalize : bool
+            Fill missing data with mean, 
+            then subtract mean and divide by standard deviation
+        """
         result = self.subtitle_languages()
         result['average_hours'] = self.average_hours()
         result['targetAudience'] = self.target_audience()
         result['n_rating'] = self.n_rating()
+        if normalize:
+            for col in ['average_hours', 'targetAudience']:
+                result[col] = result[col].fillna(np.mean(result[col]))
+            for col in ['average_hours', 'targetAudience', 'n_rating']:
+                result[col] = skpreprocessing.scale(
+                    result[col].astype('float64'))
+            result = result.fillna(0)
         return result
         
-    def feature_set(self):
+    def feature_set(self, **kwds):
         """
         Get current 'best' feature set--may change w/o warning
         """
-        return self.feature_set1()
+        return self.feature_set1(**kwds)
+
+    def recommend_content_based(self, theta, n_courses=5):
+        """
+        Recommend courses based on user preferences
+        
+        theta : dictionary 
+            feature weights. keys must be in feature_set().columns
+        
+        Returns DataFrame with top n_courses recommendations
+        """
+        w = pd.Series(theta)
+        scores = self.feature_set(normalize=True).multiply(w,
+            axis='columns')  # Dot product weights with features
+        # if feature is not given a weight, it will be NaN
+        scores = scores.fillna(0)  
+        scores = np.sum(scores, axis=1)  # sum over features
+        scores.sort(ascending=False)
+        scores = scores.head(n_courses)
+        result = self.Xraw.loc[scores.index]  # Get top course info
+        # Make sure we don't modify internal data        
+        result = result.copy(deep=True)
+        result['score'] = scores
+        return result
+        
